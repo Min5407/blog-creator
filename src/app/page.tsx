b74a6d0e3d62c7aa3761ec30f594c0f183f07e92
Min Young Go <gmy5407@gmail.com>
2024-08-20 15:40:46 +0900

    feat: add kinde auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <div>Nextjs Init</div>;
+import { Button } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export default async function Home() {
+  const { getUser, isAuthenticated } = getKindeServerSession();
+
+  const isLoginIn = await isAuthenticated();
+  const user = await getUser();
+  return (
+    <div>
+      <h1>Hello blog</h1>
+      {JSON.stringify(user)}
+      <Button asChild>
+        <LoginLink>Sign in</LoginLink>
+      </Button>
+      <Button asChild variant="destructive">
+        <RegisterLink>Sign up</RegisterLink>
+      </Button>
+    </div>
+  );
 }
